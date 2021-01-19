90520a9873f96590ef6bdb4eb52a56ee096ffc9f
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2021-01-19 23:11:01 +0100

    topNano_v6-1-2_2018_MC_fast_cfg.py

--- crab/gen/launch_GEN_DB.sh
+++ crab/gen/launch_GEN_DB.sh
@@ -1,80 +1,80 @@
 
-dir="/eos/vbc/user/robert.schoefbeck/gridpacks/"
+dir="/eos/vbc/user/robert.schoefbeck/gridpacks/v5/"
 
 #gridpack="WW_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WW-v4"
+#label="WW-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 50000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WZ_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WZ-v4"
+#label="WZ-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 50000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="ZZ_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ZZ-v4"
+#label="ZZ-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 50000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="ZZZ_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ZZZ-v4"
+#label="ZZZ-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 20000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WZZ_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WZZ-v4"
+#label="WZZ-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WWZ_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WWZ-v4"
+#label="WWZ-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WWW_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WWW-v4"
+#label="WWW-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 
 
 #gridpack="WA_LO_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WA_LO-v4"
+#label="WA_LO-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5  --production_label ${label} --unitsPerJob 50000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 
 #gridpack="WA_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WA-v4"
+#label="WA-v5"
 #python launch_GEN.py $@ --config gen_LO_012j_mc_93X_CP5  --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WWjj_OS_ewk_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WWjj_OS-v4"
+#label="WWjj_OS-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WWjj_SS_ewk_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WWjj_SS-v4"
+#label="WWjj_SS-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WZjj_ewk_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WZjj-v4"
+#label="WZjj-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 
-gridpack="ZA_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-label="ZA-v4"
-python launch_GEN.py $@ --config gen_LO_012j_mc_93X_CP5  --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
+#gridpack="ZA_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
+#label="ZA-v5"
+#python launch_GEN.py $@ --config gen_LO_012j_mc_93X_CP5  --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="ZAjj_ewk_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ZAjj-v4"
+#label="ZAjj-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="ZZjj_ewk_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ZZjj-v4"
+#label="ZZjj-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
 #gridpack="WAjj_ewk_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="WAjj-v4"
+#label="WAjj-v5"
 #python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 #
-#gridpack="ttg_noFullyHad_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ttg_noFullyHad-v4"
-#python launch_GEN.py $@ --config gen_LO_0j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
+gridpack="ttg_noFullyHad_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
+label="ttg_noFullyHad-v5"
+python launch_GEN.py $@ --config genToReco_LO_0j_mc_fast_10_2_22_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 
 #gridpack="ttW01j_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ttW01j-v4"
+#label="ttW01j-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 
 #gridpack="ttZ_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz"
-#label="ttZ01j-v4"
+#label="ttZ01j-v5"
 #python launch_GEN.py $@ --config gen_LO_01j_mc_93X_CP5 --production_label ${label} --unitsPerJob 5000 --totalUnits 10000000  --gridpackDir ${dir} --gridpack ${gridpack} --publish
 
